b320d4a25b7733ccdb973e0d2c1da3dfe12b1821
Валерия <valerikr.3005@gmail.com>
2025-04-17 21:04:50 +0500

    выполнила задания
    Внутри функции записала все квадраты чисел в list и возвратила его при завершении функции.
    Во втором вызове функции вывела возвращенный список.

--- process.py
+++ process.py
@@ -2,7 +2,9 @@ def process (text: str, flag: bool, number: float):
     try:
         if text == "":
             raise valueError("строка пустая, напишите что-то")
-    except
+    except:
+        pass
+    spisok = []
     for num in range(0, int(number)):
         kvad = num ** 2
         if num == 4:
@@ -12,9 +14,11 @@ def process (text: str, flag: bool, number: float):
         if kvad > 50:
             print(f"Квадрат числа {number} больше 50")
         if kvad >= 20 and kvad <= 50:
-            print(f "Квадрат числа {number} больше или равен 20 и меньше или равен 50")
+            print(f"Квадрат числа {number} больше или равен 20 и меньше или равен 50")
         else:
-            print(f "Квадрат числа {number} меньше 20")
+            print(f"Квадрат числа {number} меньше 20")
         print(f"Квадрат числа {num} = {kvad}") 
+        spisok.append(kvad)
+        return spisok
 process("sample text", True, 10.0)
 process("hello", False, 52.0)
