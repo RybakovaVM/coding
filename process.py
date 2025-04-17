5fafd05bff53296f568accd32e605344f928c06f
Валерия <valerikr.3005@gmail.com>
2025-04-17 20:20:27 +0500

    Сделала три задачи
    1) В цикл добавьте пропуск числа 4 и завершение цикла на числе 9.
    2) В цикл добавьте вывод сообщения "Квадрат числа {number} больше 50".
    3) Добавьте второй вызов функции, передавая другие значения.

--- process.py
+++ process.py
@@ -1,5 +1,12 @@
 def process (text: str, flag: bool, number: float):
-    for num in range(0, number):
-    kvad = num ** 2
+    for num in range(0, int(number)):
+        kvad = num ** 2
+        if num == 4:
+            continue
+        if num == 9:
+            break
+        if kvad > 50:
+            print(f"Квадрат числа {number} больше 50")
         print(f"Квадрат числа {num} = {kvad}") 
 process("sample text", True, 10.0)
+process("hello", False, 52.0)
