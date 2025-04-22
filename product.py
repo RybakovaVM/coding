920f251d808238f94fdee3eb373609fdf4100ed3
Валерия <valerikr.3005@gmail.com>
2025-04-22 19:16:25 +0500

    Написала функцию
    Написала функцию которая выведет список названий товаров, цена которых менее 80 рублей.

--- product.py
+++ product.py
@@ -2,4 +2,11 @@ products = [("Хлеб", 40), ("Молоко", 60), ("Яблоки", 100)]
 for product in products:
     name, price = product
     print(f"Товар: {name}, Цена: {price}")
- 
+    
+def find_product(products):
+    print("Товары дешевле 80 рублей:")
+    for name, price in products:
+        if price < 80:
+            print(name)
+    
+find_product(products)
