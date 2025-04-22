9e21429d403a569de84267eb7d9a84867381a7d7
Валерия <valerikr.3005@gmail.com>
2025-04-22 21:10:42 +0500

    Написала функцию
    функция для поиска товара с наибольшей общей стоимостью (цена × количество) и выведите его название и сумму.

--- product.py
+++ product.py
@@ -8,11 +8,27 @@ def find_product(products):
     for name, price, kol in products:
         if price < 80:
             print(name)
-            
+
 def calc():
     for name, price, kol in products:
         result = price * kol
         print(f"{name} — всего на {result} рублей")
-    
+        
+price = []
+def search():
+    global price
+    for product in products:
+       result = int(product[1]) * int(product[2])
+       price.append(result)
+       price.sort()
+       price.reverse()
+       print(price)
+       for product in products:
+           if (int(product[1]) * int(product[2])) == price[0]:
+            name = product[0]
+            if (int(product[1]) * int(product[2])) == price[0]:
+                print(f"Самая большая цена у товара {name} - {price[0]}")   
+
 find_product(products)
 calc()
+search()
