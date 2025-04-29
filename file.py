23c75c3138e0c689d26c1010f9c7ff408740e4e9
Валерия <valerikr.3005@gmail.com>
2025-04-29 19:40:00 +0500

    прописала функцию
    функция для прочтения файла с исключением

--- file.py
+++ file.py
@@ -1,3 +1,10 @@
-def add(out.txt):
-    with open('out.txt', 'a') as f:
+def add(outtxt):
+    with open('outtxt', 'a') as f:
         f.write("hello.\n")
+    
+def readd(outtxt):
+    try:
+        with open('outtxt', 'r') as f:
+            print(f.read())
+    except FileNotFoundError:
+        print('файл не найден')
