c9f1139477a1b16a322f0d8ce6f2df14976e4545
Валерия <valerikr.3005@gmail.com>
2025-07-08 19:32:54 +0500

    test_unit.py
     Написала программу, которая содержит следующие методы: факториал числа, возведение в степень, вычисление формулы

---
+++ test_unit.py
@@ -0,0 +1,26 @@
+class matem:
+    @staticmethod
+    def factor(n):
+        if n < 0:
+            return None
+        result = 1 
+        for i in range(1, n + 1):
+            result *= i 
+        return result
+    
+    @staticmethod        
+    def power(a, b):
+        return a ** b
+    
+    @staticmethod        
+    def sum_of_squares(n):
+            return sum(i ** 2 for i in range(1, n + 1))
+            
+if __name__ == "__main__":
+    num = 5
+    print(f"факториал числа {num} равен {matem.factor(num)}")
+    a = 2
+    b = 3
+    print(f"{a} в степени {b} равен {matem.power(a, b)}")
+    n = 4
+    print(f"сумма квадратов числа от 1 до {n} равна {matem.sum_of_squares(n)}")
